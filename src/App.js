58b7e03fd6f03da439cffe1275389c551eb37f50
Lee O'Connell <lee_oc@icloud.com>
2021-08-08 23:47:35 +0100

    feat(footer): Created footer component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import SortingVisualizer from "./components/SortingVisualizer/SortingVisualizer";
 import Navbar from "./components/Navbar/Navbar";
+import Footer from "./components/Footer/Footer";
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
         <SortingVisualizer />
 
       </div>
+      <Footer />
     </div>
   );
 }
